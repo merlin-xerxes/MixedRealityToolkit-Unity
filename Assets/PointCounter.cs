2211677618c689419789b058d2df25d74ff90838
merlin-xerxes <vincens.uibel@gmail.com>
2022-08-03 11:32:10 +0200

    MarbleScene created

---
+++ Assets/PointCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCounter : MonoBehaviour
+{
+    int pointCounter = 0;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.name == "marble") {
+            pointCounter++;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.name == "marble") {
+            pointCounter--;
+        }
+    }
+}
