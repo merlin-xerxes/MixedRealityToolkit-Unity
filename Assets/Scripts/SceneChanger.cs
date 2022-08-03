20b8c1779b55724503e84e6833194f31d6664259
Tjaardo <tjaard.pfitzner@gmail.com>
2022-08-03 19:15:34 +0200

    SceneChanger reimplemented
    somtimes it works, sometimes it doesnt.

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.SceneSystem;
+using Microsoft.MixedReality.Toolkit;
+using UnityEngine.SceneManagement;
+using System;
 
 public class SceneChanger : MonoBehaviour
 {
-    IMixedRealitySceneSystem sceneSystem = MixedRealityToolkit.Instance.GetService<IMixedRealitySceneSystem>();
-    public async void ReloadCurrentScene()
+
+    //IMixedRealitySceneSystem sceneSystem;
+    
+    
+    public void ReloadCurrentScene()
     {
-       // SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
-      await sceneSystem.LoadContent();
+        //string currentScene = SceneManager.GetActiveScene().name;
+        //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        //SceneManager.LoadSceneAsync("StartMenu");
+        //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        //await sceneSystem.LoadContent(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, LoadSceneMode.Single);
     }
 
     public void LoadScenebyName(string name)
     {
-        //  SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
-      await sceneSystem.LoadContent(name);
+        
+        //Debug.Log("aktuelle Szene: " + SceneManager.GetActiveScene().name);
+        //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        //await sceneSystem.LoadContent(name, LoadSceneMode.Single);
+        SceneManager.LoadScene(name);
+        Debug.Log("Szene erfolgreich geladen");
+        //SceneManager.SetActiveScene(SceneManager.GetSceneByName(name));
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+            
+        Debug.Log("Wechsel abgeschlossen");
 
     }
 
